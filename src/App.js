f2e33584d5b01067536b3bee5096e4d1500fc703
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-21 17:52:42 -0300

    feat: adiciona tag Em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      <h1>Em desenvolvimento</h1>
       <StrangerThings />
     </div>
   );
