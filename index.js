9ef07d5fb8c7e93410299bf19ba057e35147dcf6
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-21 15:02:41 -0300

    feat: adiciona variavel de ambiente

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const { PORT_ENV, UPSIDEDOWN_MODE } = process.env;
+
 const express = require('express');
 const cors = require('cors');
 
@@ -16,7 +19,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = (UPSIDEDOWN_MODE === 'true' ? true : false) || true;
+const PORT = PORT_ENV || 4000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +31,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
