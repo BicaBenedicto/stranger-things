8a63f3d3bfe082fc2770ab87486ed4d144bd2d72
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-21 17:36:46 -0300

    feat: adiciona variavel de ambiente a aplicaçao

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,19 +1,23 @@
+import 'dotenv/config';
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
+
+const { REACT_APP_HAWKINS_URL, REACT_APP_HAWKINS_TIMEOUT,
+  REACT_APP_UPSIDEDOWN_URL, REACT_APP_UPSIDEDOWN_TIMEOUT } = process.env;
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: REACT_APP_HAWKINS_URL,
+  timeout: REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: REACT_APP_UPSIDEDOWN_URL,
+  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
